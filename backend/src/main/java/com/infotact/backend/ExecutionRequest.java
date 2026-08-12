dd3275ca09beac1e2361e233934162400b8a8ba5
Mohammadraihan-flow <mohammadraihanhawaldar19@gmail.com>
2026-08-12 23:15:27 +0530

    feat: add polyglot code execution backend

---
+++ backend/src/main/java/com/infotact/backend/ExecutionRequest.java
@@ -0,0 +1,23 @@
+package com.infotact.backend;
+
+public class ExecutionRequest {
+
+    private String language;
+    private String code;
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
